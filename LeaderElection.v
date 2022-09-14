408f3a8e8632b21a74dd61fb2089f02f72969336
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-09-14 15:19:16 +0300

    checkpoint.

--- LeaderElection.v
+++ LeaderElection.v
@@ -34,54 +34,48 @@ Definition node_recv (s : node_st) m :=
 
 (* Definition node_recv (s : node_st)  *)
 
-Record protocol_st :=
- { queues : {ffun 'I_n -> seq nat}
- ; node_sts : {ffun 'I_n -> node_st}
+Record cprotocol_st :=
+ { cqueues : {ffun 'I_n -> seq nat}
+ ; cnode_sts : {ffun 'I_n -> node_st}
  }.
 
-Definition set_send (s : protocol_st) i j : protocol_st :=
-  {| queues := s.(queues)
-   ; node_sts := update s.(node_sts) i (fun pst => set_send_node pst j)
+Definition set_send (s : cprotocol_st) (i j : 'I_n) : cprotocol_st :=
+  {| cqueues := update s.(cqueues) j (fun pq => [:: val i & pq])
+   ; cnode_sts := update s.(cnode_sts) i (fun pst => set_send_node pst j)
   |}.
 
-Definition set_queue (s : protocol_st) i j :=
-  {| node_sts := s.(node_sts)
-   ; queues := update s.(queues) j (fun pq => [:: i & pq])
-  |}.
-
-Definition do_recv (s : protocol_st) m i :=
-  {| node_sts := update s.(node_sts) i (fun pst => node_recv pst m)
-   ; queues := update s.(queues) i (fun pq => rem m pq)
+Definition do_recv (s : cprotocol_st) m i :=
+  {| cnode_sts := update s.(cnode_sts) i (fun pst => node_recv pst m)
+   ; cqueues := update s.(cqueues) i (fun pq => rem m pq)
    |}.
 
-Definition get_max (s : protocol_st) i :=
-  (s.(node_sts) i).(max).
+Definition get_max (s : cprotocol_st) i :=
+  (s.(cnode_sts) i).(max).
 
 Inductive label := Eps | Decided of nat.
 
 Definition get_label s i :=
-  if (s.(node_sts) i).(rcount) < n
+  if (s.(cnode_sts) i).(rcount) < n
   then Eps
   else Decided (get_max s i).
 
-Reserved Notation "s1 --[ l ]--> s2" (at level 30).
+Reserved Notation "s1 --[ l ]-->_c s2" (at level 30).
 
-Inductive concrete_step : protocol_st -> label -> protocol_st -> Prop :=
+Inductive concrete_step : cprotocol_st -> label -> cprotocol_st -> Prop :=
   | Send :
     forall s1 i j,
-      (s1.(node_sts) i).(has_sent) j = false ->
+      (s1.(cnode_sts) i).(has_sent) j = false ->
       let s2 := set_send s1 i j in
-      let s2 := set_queue s2 i j in
    (* ---------------------------- *)
-      s1 --[Eps]--> s2
+      s1 --[Eps]-->_c s2
 
   | Recv :
     forall s1 i (m : 'I_n),
       (* (s1.(node_sts) i).(rcount).+1 < n -> *)
-      val m \in s1.(queues) i ->
+      val m \in s1.(cqueues) i ->
       let s2 := do_recv s1 i m in
    (* ---------------------------- *)
-      s1 --[get_label s2 i]--> s2
+      s1 --[get_label s2 i]-->_c s2
 
   (* | Decide : *)
   (*   forall s1 i (m : 'I_n), *)
@@ -91,6 +85,50 @@ Inductive concrete_step : protocol_st -> label -> protocol_st -> Prop :=
   (*  (* ---------------------------- *) *)
   (*     s1 --[Decided (get_max s2 i) ]--> s2 *)
 
-where "s1 --[ l ]--> s2" := (concrete_step s1 l s2).
+where "s1 --[ l ]-->_c s2" := (concrete_step s1 l s2).
 
-About count.
+Inductive Phase := SendP of nat | RecvP of nat.
+
+Record aprotocol_st :=
+ { phase : Phase
+ (* ; step_i : nat *)
+ (* ; step_j : nat *)
+ ; aqueues : {ffun 'I_n -> seq nat}
+ ; anode_sts : {ffun 'I_n -> node_st}
+ }.
+
+Definition set_phase s p :=
+  {| phase := p
+   ; aqueues := s.(aqueues)
+   ; anode_sts := s.(anode_sts)
+   |}.
+
+Definition phase_next p (hsent : {ffun 'I_n -> bool}):=
+  match p with
+  | SendP i =>
+      if [forall i, hsent i == true] then
+        if i < n
+        then SendP i.+1
+        else RecvP 0
+      else SendP i
+  | RecvP i => RecvP i.+1
+  end.
+
+Definition set_asend (s : aprotocol_st) (i j : 'I_n) : aprotocol_st :=
+  {| phase := phase_next s.(phase) (s.(anode_sts) i).(has_sent)
+   ; aqueues := update s.(aqueues) j (fun pq => [:: val i & pq])
+   ; anode_sts := update s.(anode_sts) i (fun pst => set_send_node pst j)
+  |}.
+
+Reserved Notation "s1 --[ l ]-->_a s2" (at level 30).
+
+Inductive abs_step : aprotocol_st -> label -> aprotocol_st -> Prop :=
+  | A_Send :
+    forall s1 (i : 'I_n) j,
+      s1.(phase) = SendP i ->
+      (s1.(anode_sts) i).(has_sent) j = false ->
+      let s2 := set_asend s1 i j in
+   (* ---------------------------- *)
+      s1 --[Eps]-->_a s2
+
+where "s1 --[ l ]-->_a s2" := (abs_step s1 l s2).
